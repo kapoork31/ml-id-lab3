077eb4d14ef338d662793bf7fcaa6285153078c1
Kunal Kapoor <kunal.d@hotmail.co.uk>
2020-10-22 18:54:16 +0100

    loading npy data

--- Source/train.py
+++ Source/train.py
@@ -127,7 +127,6 @@ estimator = TensorFlow(
                        train_instance_type=train_instance_type,
                        train_instance_count=instance_count,
                        hyperparameters=hyperparameters,
-                       role=sagemaker.get_execution_role(),
                        base_job_name='tf-fizzyo-breaths',
                        framework_version='2.2',
                        distributions = distributions,
